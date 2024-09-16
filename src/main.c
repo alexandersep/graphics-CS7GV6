100f73a50133ef8442c12471947f1dfeaa97e139
alexander <sepelena@tcd.ie>
2024-09-16 18:03:12 +0100

    Multiple lights completed

--- src/main.c
+++ src/main.c
@@ -60,6 +60,13 @@ vec3 cubePositions[] = {
     {-1.3f,  1.0f, -1.5f}
 };
 
+vec3 pointLightPositions[] = {
+    { 0.7f,  0.2f,  2.0f},
+    { 2.3f, -3.3f, -4.0f},
+    {-4.0f,  2.0f, -12.0f},
+    { 0.0f,  0.0f, -3.0f}
+};
+
 int main() {
     Camera camera;
     cg_control_camera_create(&camera, 10.0f);
@@ -136,29 +143,64 @@ int main() {
         mat4 projection;
         glm_perspective(glm_rad(camera.zoom), (float) CG_SCREEN_X / (float) CG_SCREEN_Y, 0.1f, 100.0f, projection);
 
-        vec3 lightPos = {1.2f, 1.0f, 2.0f};
         {
             cg_shader_use(shaderId);
+            cg_shader_uniform1f(shaderId, "material.shininess", 32.0f);
             cg_shader_uniform3f(shaderId, "viewPos", camera.pos[0], camera.pos[1], camera.pos[2]);
 
             cg_shader_uniform_matrix4fv(shaderId, "view", &view);
             cg_shader_uniform_matrix4fv(shaderId, "projection", &projection);
             cg_shader_uniform3f(shaderId, "objectColor", 1.0f, 1.0f, 1.0f);
 
-            cg_shader_uniform3f(shaderId, "light.ambient", 0.2f, 0.2f, 0.2f);
-            cg_shader_uniform3f(shaderId, "light.diffuse", 0.5f, 0.5f, 0.5f);
-            cg_shader_uniform3f(shaderId, "light.specular", 1.0f, 1.0f, 1.0f);
+            cg_shader_uniform3f(shaderId, "dirLight.direction", -0.2f, -1.0f, -0.3f);
+            cg_shader_uniform3f(shaderId, "dirLight.ambient", 0.05f, 0.05f, 0.05f);
+            cg_shader_uniform3f(shaderId, "dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+            cg_shader_uniform3f(shaderId, "dirLight.specular", 0.5f, 0.5f, 0.5f);
 
-            cg_shader_uniform3f(shaderId, "light.position", camera.pos[0], camera.pos[1], camera.pos[2]);
-            cg_shader_uniform3f(shaderId, "light.direction", camera.front[0], camera.front[1], camera.front[2]);
-            cg_shader_uniform1f(shaderId, "light.cutOff", cos(glm_rad(12.5f)));
-            cg_shader_uniform1f(shaderId, "light.outerCutOff", cos(glm_rad(17.5f)));
+            cg_shader_uniform3f(shaderId, "pointLights[0].position", pointLightPositions[0][0], pointLightPositions[0][1], pointLightPositions[0][2]);
+            cg_shader_uniform3f(shaderId, "pointLights[0].ambient", 0.2f, 0.2f, 0.2f);
+            cg_shader_uniform3f(shaderId, "pointLights[0].diffuse", 0.5f, 0.5f, 0.5f);
+            cg_shader_uniform3f(shaderId, "pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[0].constant", 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[0].linear", 0.09f);
+            cg_shader_uniform1f(shaderId, "pointLights[0].quadratic", 0.032f);
 
-            cg_shader_uniform1f(shaderId, "light.constant", 1.0f);
-            cg_shader_uniform1f(shaderId, "light.linear", 0.09f);
-            cg_shader_uniform1f(shaderId, "light.quadratic", 0.032f);
+            cg_shader_uniform3f(shaderId, "pointLights[1].position", pointLightPositions[1][0], pointLightPositions[1][1], pointLightPositions[1][2]);
+            cg_shader_uniform3f(shaderId, "pointLights[1].ambient", 0.2f, 0.2f, 0.2f);
+            cg_shader_uniform3f(shaderId, "pointLights[1].diffuse", 0.5f, 0.5f, 0.5f);
+            cg_shader_uniform3f(shaderId, "pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[1].constant", 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[1].linear", 0.09f);
+            cg_shader_uniform1f(shaderId, "pointLights[1].quadratic", 0.032f);
 
-            cg_shader_uniform1f(shaderId, "material.shininess", 32.0f);
+            cg_shader_uniform3f(shaderId, "pointLights[2].position", pointLightPositions[2][0], pointLightPositions[2][1], pointLightPositions[2][2]);
+            cg_shader_uniform3f(shaderId, "pointLIghts[2].ambient", 0.2f, 0.2f, 0.2f);
+            cg_shader_uniform3f(shaderId, "pointLIghts[2].diffuse", 0.5f, 0.5f, 0.5f);
+            cg_shader_uniform3f(shaderId, "pointLIghts[2].specular", 1.0f, 1.0f, 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[2].constant", 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[2].linear", 0.09f);
+            cg_shader_uniform1f(shaderId, "pointLights[2].quadratic", 0.032f);
+
+            cg_shader_uniform3f(shaderId, "pointLights[3].position", pointLightPositions[3][0], pointLightPositions[3][1], pointLightPositions[3][2]);
+            cg_shader_uniform3f(shaderId, "pointLights[3].ambient", 0.2f, 0.2f, 0.2f);
+            cg_shader_uniform3f(shaderId, "pointLights[3].diffuse", 0.5f, 0.5f, 0.5f);
+            cg_shader_uniform3f(shaderId, "pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[3].constant", 1.0f);
+            cg_shader_uniform1f(shaderId, "pointLights[3].linear", 0.09f);
+            cg_shader_uniform1f(shaderId, "pointLights[3].quadratic", 0.032f);
+
+            cg_shader_uniform3f(shaderId, "spotLight.position", camera.pos[0], camera.pos[1], camera.pos[2]);
+            cg_shader_uniform3f(shaderId, "spotLight.direction", camera.front[0], camera.front[1], camera.front[2]);
+            cg_shader_uniform3f(shaderId, "spotLight.ambient", 0.0f, 0.0f, 0.0f);
+            cg_shader_uniform3f(shaderId, "spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+            cg_shader_uniform3f(shaderId, "spotLight.specular", 1.0f, 1.0f, 1.0f);
+            cg_shader_uniform1f(shaderId, "spotLight.constant", 1.0f);
+            cg_shader_uniform1f(shaderId, "spotLight.linear", 0.09f);
+            cg_shader_uniform1f(shaderId, "spotLight.quadratic", 0.032f);
+            cg_shader_uniform1f(shaderId, "spotLight.cutOff", cos(glm_rad(12.5f)));
+            cg_shader_uniform1f(shaderId, "spotLight.outerCutOff", cos(glm_rad(15.0f)));     
+            cg_shader_uniform1f(shaderId, "spotLight.cutOff", cos(glm_rad(12.5f)));
+            cg_shader_uniform1f(shaderId, "spotLight.outerCutOff", cos(glm_rad(17.5f)));
 
             cg_texture_use(textureId, 0);
             cg_texture_use(textureId, 1);
@@ -184,12 +226,14 @@ int main() {
             cg_shader_uniform_matrix4fv(shaderLight, "view", &view);
 
             glBindVertexArray(VAO[1]);
-            mat4 model = GLM_MAT4_IDENTITY;
-            glm_translate_make(model, lightPos);
-            glm_scale(model, (vec3) {0.2f, 0.2f, 0.2f});
-            cg_shader_uniform_matrix4fv(shaderLight, "model", &model);
+            for (unsigned int i = 0; i < 4; i++) {
+                mat4 model = GLM_MAT4_IDENTITY;
+                glm_translate_make(model, pointLightPositions[i]);
+                glm_scale(model, (vec3) {0.2f, 0.2f, 0.2f});
+                cg_shader_uniform_matrix4fv(shaderLight, "model", &model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
         }
 
         glfwSwapBuffers(window);
